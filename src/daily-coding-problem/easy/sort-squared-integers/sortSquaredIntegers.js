71cb7e4b08a5c404bc5bf18832af42e8aab4103d
Nathan Thomas <nwthomas@me.com>
2020-01-16 20:00:00 -0600

    Add Sort Squared Integers DCP challenge solution

---
+++ src/daily-coding-problem/easy/sort-squared-integers/sortSquaredIntegers.js
@@ -0,0 +1,50 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+Given a sorted list of integers, square the elements and give the output in sorted order.
+
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+*/
+
+function squareAndSort(intArray) {
+  if (!Array.isArray(intArray)) {
+    throw new TypeError("The function argument must be an array of integers.");
+  }
+  if (!intArray.length) {
+    return intArray;
+  }
+  const _squared = intArray.map(int => int * int);
+  const _sorted = mergeSor(_squared);
+  return _sorted;
+}
+
+function mergeSort(intArray) {
+  if (!Array.isArray(intArray)) {
+    throw new TypeError("The function argument must be an array of integers.");
+  }
+  if (intArray.length <= 1) {
+    return intArray;
+  }
+  const _pivotIndex = Math.floor(Math.random() * intArray.length);
+  const left = intArray.slice(0, _pivotIndex);
+  const right = intArray.slice(_pivotIndex, intArray.length);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(arr1, arr2) {
+  const _final = [],
+    _arr1 = [...arr1],
+    _arr2 = [...arr2];
+  while (_arr1.length || _arr2.length) {
+    if (!_arr1.length || _arr2[0] <= _arr1[0]) {
+      _final.push(_arr2.shift());
+    } else if (!_arr2.length || _arr1[0] <= _arr2[0]) {
+      _final.push(_arr1.shift());
+    }
+  }
+  return _final;
+}
+
+module.exports = { squareAndSort, mergeSort, merge };
--- src/daily-coding-problem/medium/README.md
+++ src/daily-coding-problem/medium/README.md
@@ -6,6 +6,7 @@
   - [Find Non-Decreasing Integers](find-non-decreasing-integers)
   - [Find 2D Array Values](find-2d-array-values)
   - [Find Tree Paths](find-tree-paths)
+  - [Interval Spread](interval-spread)
   - [Maximum Possible Words](maximum-possible-words)
   - [Random Number Except List](random-number-except-list)
   - [Reverse Binary Tree](reverse-binary-tree)
