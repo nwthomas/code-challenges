1826f40535cf9483b7f0f069177e2a4006eabd2f
Nathan Thomas <nwthomas@me.com>
2020-01-30 22:11:04 -0800

    Add HitCounter class for DCP problem

---
+++ src/daily-coding-problem/easy/hit-counter/hitCounter.js
@@ -0,0 +1,55 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This question was asked by Riot Games.
+
+Design and implement a HitCounter class that keeps track of requests (or hits). It should support the following operations:
+
+record(timestamp): records a hit that happened at timestamp
+total(): returns the total number of hits recorded
+range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
+
+Follow-up: What if our system has limited memory?
+*/
+
+class HitCounter {
+    constructor() {
+        this.hitCounter = {};
+        this.totalHits = 0;
+    }
+
+    record(timestamp) {
+        if (
+            typeof timestamp !== "string" &&
+            typeof parseInt(timestamp) !== "number"
+        ) {
+            return null;
+        } else {
+            this.hitCounter[timestamp]
+                ? this.hitCounter[timestamp]++
+                : (this.hitCounter[timestamp] = 1);
+            this.totalHits++;
+            return { timestamp: this.hitCounter[timestamp] };
+        }
+    }
+
+    range(lower, upper) {
+        let total = 0;
+        const _lower = parseInt(lower);
+        const _upper = parseInt(upper);
+        Object.entries(this.totalHits).forEach(entry => {
+            const key = parseInt(entry[0]);
+            const value = entry[1];
+            if (key >= _lower && key <= _upper) {
+                total += value;
+            }
+        });
+        return total;
+    }
+
+    total() {
+        return this.totalHits;
+    }
+}
+
+module.exports = HitCounter;
