bc1b459a85c5579fc53641b519733ab87ce7825e
Nathan Thomas <nwthomas@me.com>
2020-03-15 20:00:00 -0600

    Baseline testing file for String Mapping DCP

---
+++ src/daily-coding-problem/easy/string-mapping/test_string_mapping.py
@@ -0,0 +1,10 @@
+from string_mapping import are_strings_mappable
+import unittest
+
+
+class TestAreStringsMappable(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
