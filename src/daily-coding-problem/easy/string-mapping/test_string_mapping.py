1360eecd85117b5103c042668c864109866e41f8
Nathan Thomas <nwthomas@me.com>
2020-03-16 20:00:00 -0600

    Add first test for String Mapping DCP

--- src/daily-coding-problem/easy/string-mapping/test_string_mapping.py
+++ src/daily-coding-problem/easy/string-mapping/test_string_mapping.py
@@ -3,7 +3,14 @@ import unittest
 
 
 class TestAreStringsMappable(unittest.TestCase):
-    pass
+    def test_throws_error_if_not_strings(self):
+        """
+        Throws a TypeError if either of the arguments are a string
+        """
+        def result_one(): return are_strings_mappable({}, "test")
+        def result_two(): return are_strings_mappable("test", {})
+        self.assertRaises(TypeError, result_one)
+        self.assertRaises(TypeError, result_two)
 
 
 if __name__ == "__main__":
