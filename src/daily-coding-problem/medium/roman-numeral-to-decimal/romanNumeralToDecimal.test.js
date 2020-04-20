f72b852bbd2aeb4bcdfa90ba3179124348343e63
Nathan Thomas <nwthomas@me.com>
2020-04-19 20:00:00 -0600

    Add some tests for DCP challenge

--- src/daily-coding-problem/medium/roman-numeral-to-decimal/romanNumeralToDecimal.test.js
+++ src/daily-coding-problem/medium/roman-numeral-to-decimal/romanNumeralToDecimal.test.js
@@ -1,5 +1,26 @@
 const convertRomanNumeralToDecimal = require("./romanNumeralToDecimal.js");
 
 describe("Roman Numeral to Decimal", () => {
-    // finish
+    test("throws an error if the type of argument is not a string", () => {
+        const result = () => convertRomanNumeralToDecimal({});
+        expect(result).toThrow(
+            TypeError(
+                "The argument for convertRomanNumeralToDecimal must be a string"
+            )
+        );
+    });
+
+    test("throws an error if one of the characters is not a Roman numeral", () => {
+        const result = () => convertRomanNumeralToDecimal("ABKGHAKHJDG");
+        expect(result).toThrow(
+            Error(
+                "All characters input into convertRomanNumeralToDecimal must be valid Roman numerals"
+            )
+        );
+    });
+
+    test("returns the correct answer for a single Roman numeral", () => {
+        const result = convertRomanNumeralToDecimal("L");
+        expect(result).toBe(50);
+    });
 });
