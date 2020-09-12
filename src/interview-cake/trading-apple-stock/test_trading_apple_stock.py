612f043f0c7eee1452f56c37c313c196fca486b8
Nathan Thomas <nwthomas@me.com>
2020-09-11 20:00:00 -0600

    Add tests for Trading Apple Stock IC

--- src/interview-cake/trading-apple-stock/test_trading_apple_stock.py
+++ src/interview-cake/trading-apple-stock/test_trading_apple_stock.py
@@ -3,7 +3,26 @@ import unittest
 
 
 class TestFindBestStockProfit(unittest.TestCase):
-    pass
+    def test_raises_typeerror_if_argument_is_not_list(self):
+        """Raises a new TypeError if the argument is not of type list"""
+        def result(): return find_best_stock_profit("test")
+        self.assertRaises(TypeError, result)
+
+    def test_returns_0_if_argument_is_empty_list(self):
+        """Returns 0 profit if the argument is an empty list"""
+        result = find_best_stock_profit([])
+        self.assertEqual(result, 0)
+
+    def test_returns_correct_profit_total(self):
+        """Returns the correct profit possible from a list of stock prices"""
+        result = find_best_stock_profit([10, 7, 5, 8, 11, 9])
+        self.assertEqual(result, 6)
+
+    def test_returns_correct_profit_total_from_large_list(self):
+        """Returns the correct profit possible from a large list of stock prices"""
+        l = [9, 10, 40, 39, 2, 45, 3, 67, 23, 10, 6, 5, 4, 4, 78, 9, 6, 100]
+        result = find_best_stock_profit(l)
+        self.assertEqual(result, 98)
 
 
 if __name__ == "__main__":
