6d8887a98e2c3da0299e11b0648bee334d896cf2
Nathan Thomas <nwthomas@me.com>
2021-04-09 23:51:43 -0700

    Write tests for successful completion of sorted SLLs challenge

--- src/daily-coding-problem/medium/sort-singly-linked-lists/sort_singly_linked_lists.py
+++ src/daily-coding-problem/medium/sort-singly-linked-lists/sort_singly_linked_lists.py
@@ -7,54 +7,103 @@ Given k sorted singly linked lists, write a function to merge all the lists into
 """
 from random import randrange
 
-class SinglyLinkedList:
+class Node:
     def __init__(self, value):
-        self.head = Node(value)
-        self.tail = self.head
-        self.length = 1
-    
+        self.value = value
+        self.next = None
+
     def add_node(self, value):
         """Adds a value to the singly linked list"""
-        current = self.head
+        current = self
 
         while current.next:
             current = current.next
 
         current.next = Node(value)
-        self.tail = current.next
-        self.length += 1
 
-class Node:
-    def __init__(self, value, next_value = None):
-        self.value = value
-        self.next = next_value
-
-    def __str__(self):
-        node_string = f"{self.value}"
-        return f"{node_string} --> {self.next}" if self.next else node_string
-
-def connect_singly_linked_lists(*args):
+def get_singly_linked_list_nodes(*args):
     """connects any number of singly linked lists together"""
-    final_list = None
+    node_dict = {}
+    values_list = []
 
-    for next_list in args:
-        if not final_list:
-            final_list = next_list
+    def add_node_to_dict(node):
+        if node.value in node_dict:
+            node_dict[node.value].append(node)
         else:
-            final_list.tail.next = next_list.head
-            final_list.tail = next_list.tail
-            final_list.length += next_list.length
+            node_dict[node.value] = [node]
 
-            # Remove all references from old list class for garbage collection
-            next_list.head = None
-            next_list.tail = None
+        values_list.append(node.value)
 
-    return final_list
+    for node in args:
+        add_node_to_dict(node)
+        current_node = node
 
+        while current_node.next:
+            # We need to clear the next reference to avoid an infinite loop when joined later
+            temp = current_node.next
+            current_node.next = None
+            current_node = temp
+            add_node_to_dict(current_node)
 
-def quick_sort_singly_linked_lists(*args):
+    return (node_dict, values_list)
+
+def quick_sort_singly_linked_lists(unsorted_node_dict, values_list):
+    """Quick sorts a group of nodes with an associated values list"""
+    if len(values_list) == 1:
+        return [node for node in unsorted_node_dict[values_list[0]]]
+    elif len(values_list) < 1:
+        return []
+    elif values_list:
+        random_index = randrange(len(values_list))
+        pivot_value = values_list[random_index]
+
+        lower_nodes = {}
+        lower_values = []
+
+        equal_nodes = []
+
+        higher_nodes = {}
+        higher_values = []
+
+        for value in values_list:
+            # This covers cases where we've already removed multiple of the same integer
+            # from the dict and removed with the delete keyword
+            if not value in unsorted_node_dict:
+                continue
+            else:
+                if value > pivot_value:
+                    higher_nodes[value] = unsorted_node_dict[value]
+                    del unsorted_node_dict[value]
+                    higher_values.append(value)
+                elif value < pivot_value:
+                    lower_nodes[value] = unsorted_node_dict[value]
+                    del unsorted_node_dict[value]
+                    lower_values.append(value)
+                else:
+                    equal_nodes += [node for node in unsorted_node_dict[value]]
+    
+        return quick_sort_singly_linked_lists(lower_nodes, lower_values) + equal_nodes + quick_sort_singly_linked_lists(higher_nodes, higher_values)
+
+def join_sorted_node_list(sorted_node_list):
+    """Takes in a sorted node list and returns a sorted singly linked list"""
+    head_node = None
+    current_node = None
+
+    for node in sorted_node_list:
+        if not head_node:
+            head_node = node
+            current_node = head_node
+        else:
+            current_node.next = node
+            current_node = current_node.next
+    
+    return head_node
+
+def sort_singly_linked_lists(*args):
     """Takes in any number of sorted Singly Linked Lists and returns one that is sorted"""
-    unsorted_list = connect_singly_linked_lists(*args)
+    unsorted_node_dict, unsorted_values_list = get_singly_linked_list_nodes(*args)
+    sorted_node_list = quick_sort_singly_linked_lists(unsorted_node_dict, values_list=unsorted_values_list)
+    sorted_singly_linked_list = join_sorted_node_list(sorted_node_list)
 
-    return unsorted_list
+    return sorted_singly_linked_list
 
--- src/daily-coding-problem/medium/sort-singly-linked-lists/test_sort_singly_linked_lists.py
+++ src/daily-coding-problem/medium/sort-singly-linked-lists/test_sort_singly_linked_lists.py
@@ -1,4 +1,4 @@
-from sort_singly_linked_lists import SinglyLinkedList, quick_sort_singly_linked_lists
+from sort_singly_linked_lists import Node, sort_singly_linked_lists
 import unittest
 
 
@@ -9,17 +9,30 @@ class TestSortSinglyLinkedLists(unittest.TestCase):
 
         for value in values_list:
             if not new_singly_linked_list:
-                new_singly_linked_list = SinglyLinkedList(value)
+                new_singly_linked_list = Node(value)
             else:
                 new_singly_linked_list.add_node(value)
 
         return new_singly_linked_list
+    
+    def get_sorted_singly_linked_lists_values(self, sorted_singly_linked_list):
+        """Takes in a sorted singly linked list and returns the values from it"""
+        current = sorted_singly_linked_list
+        final = [current.value]
 
-    def test_returns_sorted_list(self):
+        while current.next:
+            current = current.next
+            final.append(current.value)
+        
+        return final
+
+    def test_returns_single_sorted_list(self):
         """Tests that two separate sorted singly linked lists are returned as one sorted one"""
-        s1 = self.create_singly_linked_list([1, 3, 4, 6, 8])
-        s2 = self.create_singly_linked_list([2, 5, 8, 11, 12])
-        print(quick_sort_singly_linked_lists(s1, s2).head)
+        s1 = self.create_singly_linked_list([1, 3, 4, 6, 8, 40, 89, 28, 90, 100])
+        s2 = self.create_singly_linked_list([2, 5, 8, 11, 12, 100, 567, 1000])
+        s3 = self.create_singly_linked_list([1, 4, 6, 9, 10, 34, 162873, 126381672389, 123961987263])
+        result = sort_singly_linked_lists(s1, s2, s3)
+        self.assertEqual(self.get_sorted_singly_linked_lists_values(result), [1, 1, 2, 3, 4, 4, 5, 6, 6, 8, 8, 9, 10, 11, 12, 28, 34, 40, 89, 90, 100, 100, 567, 1000, 162873, 123961987263, 126381672389])
 
 if __name__ == "__main__":
     unittest.main()
