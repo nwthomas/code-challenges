746e6ccfd38755d72042d5d21e1d4d7a6b7dae46
Nathan Thomas <nwthomas@me.com>
2021-07-05 11:22:55 -0700

    Finish writing tests for new DCP easy challenge

--- src/daily-coding-problem/easy/find-friend-groups/test_find_friend_group.py
+++ src/daily-coding-problem/easy/find-friend-groups/test_find_friend_group.py
@@ -6,7 +6,14 @@ class TestFindFriendGroups(unittest.TestCase):
         """Takes in an adjacency list of friends and returns 2"""
         adj_list = {0: [1, 2], 1: [2], 2: [0], 3: []}
         result = find_friend_groups(adj_list)
+        self.assertEqual(result, 2)
+
+    def test_returns_three_groups(self):
+        """Takes in an adjacency list of friends and returns 3"""
+        adj_list = {0: [1, 2], 1: [0, 5], 2: [0], 3: [6], 4: [], 5: [1], 6: [3]}
+        result = find_friend_groups(adj_list)
         self.assertEqual(result, 3)
+
 
 if __name__ == "__main__":
     unittest.main()
