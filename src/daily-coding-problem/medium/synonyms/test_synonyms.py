45f25de8b743e47396faa37dcbf6da0d9008b45d
Nathan Thomas <nwthomas@me.com>
2021-12-06 20:00:00 -0600

    Scaffold testing file

---
+++ src/daily-coding-problem/medium/synonyms/test_synonyms.py
@@ -0,0 +1,9 @@
+from synonyms import are_sentences_equivalent
+import unittest
+
+class TestSynonyms(unittest.TestCase):
+    def test_are_sentences_similar(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
