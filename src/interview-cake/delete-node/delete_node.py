2ce4dc74357e9ea983d40608ac0296bf6e8ad74f
Nathan Thomas <nwthomas@me.com>
2022-01-01 20:00:00 -0600

    Write draft code for code challenge

--- src/interview-cake/delete-node/delete_node.py
+++ src/interview-cake/delete-node/delete_node.py
@@ -18,3 +18,37 @@ b.next = c
 
 delete_node(b)
 """
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node_to_list(self, value):
+        """Adds a new node to the SLL"""
+        if not self.head:
+            self.head = Node(value)
+        else:
+            current = self.head
+
+            while current.next:
+                current = current.next
+            
+            current.next = Node(value)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def delete_node(node):
+    """Takes in a node and deletes it from a chain where it is not the end node"""
+    next_node = node.next
+    node.value = next_node.value
+    node.next = next_node.next
+
+    # Allows garbage collection of node with no reference/use
+    next_node.next = None
+
+
+
+
