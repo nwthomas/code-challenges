d210368635c2fb1d9253e65a2cd3a258f440cd87
Nathan Thomas <nwthomas@me.com>
2022-03-01 20:00:00 -0600

    Add draft test template for hourglass sum

---
+++ src/hacker-rank/easy/hourglass_sum/test_hourglass_sum.py
@@ -0,0 +1,9 @@
+from hourglass_sum import hourglassSum
+import unittest
+
+class TestHourglassSum(unittest.TestCase):
+    def test_returns_largest_sum(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
