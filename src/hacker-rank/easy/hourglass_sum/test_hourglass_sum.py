de3d7075a6bc05b4ff3e52589442b66e8870696f
Nathan Thomas <nwthomas@me.com>
2022-03-03 20:00:00 -0600

    Add first test case hourglass sum

--- src/hacker-rank/easy/hourglass_sum/test_hourglass_sum.py
+++ src/hacker-rank/easy/hourglass_sum/test_hourglass_sum.py
@@ -2,8 +2,17 @@ from hourglass_sum import hourglassSum
 import unittest
 
 class TestHourglassSum(unittest.TestCase):
+    small_2d_array = [
+        [1, 2, 3, 4],
+        [0, 9, 8, 7],
+        [3, 5, 1, 2],
+        [10, 5, 9, 2],
+        [8, 9, 3, 4]
+    ]
     def test_returns_largest_sum(self):
-        pass
+        """Takes in a 2D array and returns the largest hourglass sum within it"""
+        result = hourglassSum(self.small_2d_array)
+        self.assertEqual(result, 46)
 
 if __name__ == "__main__":
     unittest.main()
