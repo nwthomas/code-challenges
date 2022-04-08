e4b58376a052d5cde04a570f1d33d38f32460886
Nathan Thomas <nwthomas@me.com>
2022-04-08 10:50:43 -0700

    Add anagram test

--- src/hacker-rank/medium/sherlock-and-anagrams/test_sherlock_and_anagrams.py
+++ src/hacker-rank/medium/sherlock-and-anagrams/test_sherlock_and_anagrams.py
@@ -7,5 +7,10 @@ class TestSherlockAndAnagrams(unittest.TestCase):
         result = sherlock_and_anagrams("abba");
         self.assertEqual(result, 4)
 
+    def test_returns_count_zero_when_not_anagram(self):
+        """Returns 0 as the correct count when an anagram is not possible"""
+        result = sherlock_and_anagrams("abcd")
+        self.assertEqual(result, 0)
+
 if __name__ == "__main__":
     unittest.main()
