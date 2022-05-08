74100c719cb5886e1d500de4945c5f2431090aed
Nathan Thomas <nwthomas@me.com>
2022-05-08 15:19:51 -0500

    Add tests for search in rotated array

--- src/leetcode/medium/search-in-rotated-array/test_search_in_rotated_array.py
+++ src/leetcode/medium/search-in-rotated-array/test_search_in_rotated_array.py
@@ -2,8 +2,30 @@ from search_in_rotated_array import search
 import unittest
 
 class TestSearchInRotatedArray(unittest.TestCase):
-    def test_works_for_empty_array(self):
-        pass
+    def test_returns_neg_one_for_empty_array(self):
+        """Takes in an empty array and returns -1 for searched number"""
+        result = search([], 0)
+        self.assertEqual(result, -1)
+
+    def test_returns_neg_one_for_short_array(self):
+        """Takes in a short array and returns -1 for searched number"""
+        result = search([0, 1], 10)
+        self.assertEqual(result, -1)
+
+    def test_returns_index_for_short_array(self):
+        """Takes in a short array and returns the index for searched number"""
+        result = search([10, 0], 0)
+        self.assertEqual(result, 1)
+
+    def test_returns_index_for_rotate_array(self):
+        """Takes in a rotated array and returns the index for the searched number"""
+        result = search([5, 6, 7, 0, 1, 2, 3, 4], 2)
+        self.assertEqual(result, 5)
+
+    def test_returns_index_for_unrotated_array(self):
+        """Takes in a non-rotated array and returns the index for the searched number"""
+        result = search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
+        self.assertEqual(result, 2)
 
 if __name__ == "__main__":
     unittest.main()
