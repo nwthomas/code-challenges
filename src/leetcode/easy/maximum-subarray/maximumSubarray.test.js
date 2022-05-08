7cc0bcc067b15a65f1824403696393616e38813c
Nathan Thomas <nwthomas@me.com>
2022-05-08 15:34:22 -0500

    Add tests for maximum sub array

--- src/leetcode/easy/maximum-subarray/maximumSubarray.test.js
+++ src/leetcode/easy/maximum-subarray/maximumSubarray.test.js
@@ -1,5 +1,20 @@
 const { maxSubArray } = require("./maximumSubarray.js");
 
 describe("maxSubArray", () => {
-    // finish
+    test("returns the value for an array of length 1", () => {
+        const result = maxSubArray([10]);
+        expect(result).toBe(10);
+    });
+
+    test("returns the value for a short array", () => {
+        const result = maxSubArray([1, 4, 28, -100, 4, 40, 13]);
+        expect(result).toBe(57);
+    });
+
+    test("returns the value for a long array", () => {
+        const result = maxSubArray([
+            1, 4, 28, -100, 4, 40, 13, 1000, -200, -400, 500, 3, -20, -300, 100,
+        ]);
+        expect(result).toBe(1057);
+    });
 });
