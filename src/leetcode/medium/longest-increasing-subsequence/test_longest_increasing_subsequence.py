26806b9a14b3452bdcc522a3004a59c7d44287c6
Nathan Thomas <nwthomas@me.com>
2022-05-21 20:46:56 -0700

    Add longest increasing subsequence

---
+++ src/leetcode/medium/longest-increasing-subsequence/test_longest_increasing_subsequence.py
@@ -0,0 +1,8 @@
+from longest_increasing_subsequence import get_longest_increasing_subsequence
+import unittest
+
+class TestGetLongestIncreasingSubsequence(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
