f2541f9b29ec1f5a11108ea48c305ecd789fd503
Nathan Thomas <nwthomas@me.com>
2022-05-27 17:38:41 -0700

    Add testing suite for spiral order LC

--- src/leetcode/medium/spiral-matrix/test_spiral_matrix.py
+++ src/leetcode/medium/spiral-matrix/test_spiral_matrix.py
@@ -2,7 +2,15 @@ from spiral_matrix import spiral_order
 import unittest
 
 class TestSpiralOrder(unittest.TestCase):
-    pass
+    def test_outputs_list_of_one(self):
+        """Takes in a list with one array of one integer and returns it"""
+        result = spiral_order([[0]])
+        self.assertEqual(result, [0])
+
+    def test_returns_correct_spiral_order(self):
+        """Takes in a 2D matrix and returns a list with the integers in spiral order"""
+        result = spiral_order([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        self.assertEqual(result, [1, 2, 3, 6, 9, 8, 7, 4, 5])
 
 if __name__ == "__main__":
     unittest.main()
