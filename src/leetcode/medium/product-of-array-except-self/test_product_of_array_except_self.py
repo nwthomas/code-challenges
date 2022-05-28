d081c46e3f945be80d8cd0d7d701b4f0fbe0e685
Nathan Thomas <nwthomas@me.com>
2022-05-27 17:32:59 -0700

    Add testing suite for Python product of array except self

--- src/leetcode/medium/product-of-array-except-self/test_product_of_array_except_self.py
+++ src/leetcode/medium/product-of-array-except-self/test_product_of_array_except_self.py
@@ -1,8 +1,17 @@
+from math import prod
 from product_of_array_except_self import product_except_self
 import unittest
 
 class TestProductExceptSelf(unittest.TestCase):
-    pass
+    def test_returns_empty_list_for_empty_list(self):
+        """Takes in an empty list and returns an empty one"""
+        result = product_except_self([])
+        self.assertEqual(result, [])
+
+    def test_returns_correct_product_list(self):
+        """Takes in a list of integers and returns the correct product list"""
+        result = product_except_self([4, 7, 1, 1, 2])
+        self.assertEqual(result, [14, 8, 56, 56, 28])
 
 if __name__ == "__main__":
     unittest.main()
