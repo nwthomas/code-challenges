bcfc4bd75d216a96240da76495fe56cfecd3091b
Nathan Thomas <nwthomas@me.com>
2022-05-27 17:20:15 -0700

    Add tests for jump game II LC

--- src/leetcode/medium/jump-game-ii/test_jump_game_ii.py
+++ src/leetcode/medium/jump-game-ii/test_jump_game_ii.py
@@ -2,7 +2,16 @@ from jump_game_ii import can_jump
 import unittest
 
 class TestCanJump(unittest.TestCase):
-    pass
+    def test_returns_correct_jump_for_short_list(self):
+        """Returns the correct minimum number of jumps for a short list"""
+        result = can_jump([3, 1, 1, 2, 6, 3])
+        self.assertEqual(result, 2)
+
+    def test_returns_correct_jumps_for_long_list(self):
+        """Returns the correct minimum number of jumps for a long list"""
+        result = can_jump([5, 1, 1, 1, 1, 1, 1, 1, 7, 2, 3, 2, 1, 1, 3, 0, 5, 5, 1, 1, 1, 1, 1])
+        self.assertEqual(result, 7)
+
 
 if __name__ == "__main__":
     unittest.main()
