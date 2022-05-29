ccdc763718cd9e333973c758e31252b0b854d197
Nathan Thomas <nwthomas@me.com>
2022-05-29 16:58:30 -0700

    Fix comment descriptions for LC easy challenges

--- src/leetcode/easy/remove-element/remove_element.py
+++ src/leetcode/easy/remove-element/remove_element.py
@@ -28,13 +28,12 @@ for (int i = 0; i < actualLength; i++) {
 If all assertions pass, then your solution will be accepted.
 
 Example 1:
-
 Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores).
-Example 2:
 
+Example 2:
 Input: nums = [0,1,2,2,3,0,4,2], val = 2
 Output: 5, nums = [0,1,4,0,3,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
@@ -42,7 +41,6 @@ Note that the five elements can be returned in any order.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
 Constraints:
-
 0 <= nums.length <= 100
 0 <= nums[i] <= 50
 0 <= val <= 100
--- src/leetcode/easy/valid-parentheses/valid_parentheses.py
+++ src/leetcode/easy/valid-parentheses/valid_parentheses.py
@@ -9,20 +9,18 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 
 Example 1:
-
 Input: s = "()"
 Output: true
-Example 2:
 
+Example 2:
 Input: s = "()[]{}"
 Output: true
-Example 3:
 
+Example 3:
 Input: s = "(]"
 Output: false
 
 Constraints:
-
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 """
