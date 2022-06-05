a49ed133132200d22b85f104390932d9f3a43e77
Nathan Thomas <nwthomas@me.com>
2022-06-04 19:01:45 -0700

    Add tests for top k freq nums LC

--- src/leetcode/medium/top-k-frequent-elements/test_top_k_frequent_elements.py
+++ src/leetcode/medium/top-k-frequent-elements/test_top_k_frequent_elements.py
@@ -2,7 +2,15 @@ from top_k_frequent_elements import top_k_frequent
 import unittest
 
 class TopKFrequent(unittest.TestCase):
-    pass
+    def test_returns_empty_list_for_not_frequent_nums(self):
+        """Takes in an empty list and returns an empty list"""
+        result = top_k_frequent([], 2)
+        self.assertEqual(result, [])
+
+    def test_returns_empty_list_for_nth_frequent_nums(self):
+        """Takes in a list of nums and returns the nth most frequent nums"""
+        result = top_k_frequent([4, 1, 6, 2, 8, 3, 9, 4, 3, 1], 3)
+        self.assertEqual(result, [1, 3, 4])
 
 if __name__ == "__main__":
     unittest.main()
