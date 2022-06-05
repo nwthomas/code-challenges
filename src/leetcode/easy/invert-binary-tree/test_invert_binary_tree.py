8e77cc05287b390ddb93678b7b86caba23551b7c
Nathan Thomas <nwthomas@me.com>
2022-06-04 18:33:01 -0700

    Add test for invert binary tree

--- src/leetcode/easy/invert-binary-tree/test_invert_binary_tree.py
+++ src/leetcode/easy/invert-binary-tree/test_invert_binary_tree.py
@@ -1,8 +1,17 @@
-from invert_binary_tree import invert_tree
+from invert_binary_tree import invert_tree, TreeNode
 import unittest
 
 class TestInvertTree(unittest.TestCase):
-    pass
+    def test_inverts_binary_tree(self):
+        """Takes in a tree and inverts it"""
+        t = TreeNode(10)
+        t.left = TreeNode(4)
+        t.right = TreeNode(15)
+
+        result = invert_tree(t)
+        self.assertEqual(result.val, 10)
+        self.assertEqual(result.right.val, 4)
+        self.assertEqual(result.left.val, 15)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
