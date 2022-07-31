fab2eabccd0ac3e7cc9a3e118a190ca4e872f8c4
Nathan Thomas <nwthomas@me.com>
2022-07-31 14:35:47 -0700

    Add tests for longest common palindrome subsequence

--- src/leetcode/medium/longest-palindrome-subsequence/test_longest_palindrome_subsequence.py
+++ src/leetcode/medium/longest-palindrome-subsequence/test_longest_palindrome_subsequence.py
@@ -2,7 +2,15 @@ from longest_palindrome_subsequence import get_longest_palindrome_subsequence
 import unittest
 
 class TestGetLongestPalindromeSubsequence(unittest.TestCase):
-    pass
+    def test_returns_longest_common_palindrome_subsequence(self):
+        """Takes in a string and returns the longest common palindrome subsequence"""
+        result = get_longest_palindrome_subsequence("anagram")
+        self.assertEqual(result, 3)
+
+    def test_returns_one_if_no_common_palindrome_subsequence(self):
+        """Takes in a string and returns 1 for 1 char if no common palindrome subsequence"""
+        result = get_longest_palindrome_subsequence("abcxyz")
+        self.assertEqual(result, 1)
 
 if __name__ == "__main__":
     unittest.main()
