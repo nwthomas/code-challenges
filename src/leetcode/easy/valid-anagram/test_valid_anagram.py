7ff6440dd37930765cb8df73b38e7dc8bd4e6c7e
Nathan Thomas <nwthomas@me.com>
2022-07-31 13:31:44 -0700

    Add valid anagram tests

--- src/leetcode/easy/valid-anagram/test_valid_anagram.py
+++ src/leetcode/easy/valid-anagram/test_valid_anagram.py
@@ -1,8 +1,16 @@
-from is_anagram import is_anagram
+from valid_anagram import is_anagram
 import unittest
 
 class TestIsAnagram(unittest.TestCase):
-    pass
+    def test_returns_true_if_anagram(self):
+        """Returns True if words are anagrams"""
+        result = is_anagram("anagram", "nagaram")
+        self.assertTrue(result)
+
+    def test_returns_false_if_not_anagram(self):
+        """Returns False if words are not anagrams"""
+        result = is_anagram("testing", "graham")
+        self.assertFalse(result)
 
 if __name__ == "__main__":
     unittest.main()
