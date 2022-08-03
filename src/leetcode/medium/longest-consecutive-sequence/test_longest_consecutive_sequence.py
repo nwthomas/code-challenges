0ed6c09d8c7e3ae26263babd4acbdd9b43043c76
Nathan Thomas <nwthomas@me.com>
2022-08-02 21:32:14 -0700

    Add tests for longest consecutive sequence

---
+++ src/leetcode/medium/longest-consecutive-sequence/test_longest_consecutive_sequence.py
@@ -0,0 +1,37 @@
+from longest_consecutive_sequence import longest_consecutive_with_heap, longest_consecutive_with_set
+import unittest
+
+class TestLongestConsecutiveWithHeap(unittest.TestCase):
+    def test_returns_correct_longest_consecutive_sequence_total(self):
+        """Takes in an unsorted list of numbers and returns correct total"""
+        result = longest_consecutive_with_heap([3, 100, 4, 5, 23, 6, 19, 7])
+        self.assertEqual(result, 5)
+
+    def test_returns_zero_if_empty_list(self):
+        """Takes in empty list and returns 0 for the total"""
+        result = longest_consecutive_with_heap([])
+        self.assertEqual(result, 0)
+
+    def test_returns_one_for_single_number(self):
+        """Takes in a list with one number and returns the correct total"""
+        result = longest_consecutive_with_heap([100])
+        self.assertEqual(result, 1)
+
+class TestLongestConsecutiveWithSet(unittest.TestCase):
+    def test_returns_correct_longest_consecutive_sequence_total(self):
+        """Takes in an unsorted list of numbers and returns correct total"""
+        result = longest_consecutive_with_set([3, 100, 4, 5, 23, 6, 19, 7])
+        self.assertEqual(result, 5)
+
+    def test_returns_zero_if_empty_list(self):
+        """Takes in empty list and returns 0 for the total"""
+        result = longest_consecutive_with_set([])
+        self.assertEqual(result, 0)
+
+    def test_returns_one_for_single_number(self):
+        """Takes in a list with one number and returns the correct total"""
+        result = longest_consecutive_with_set([100])
+        self.assertEqual(result, 1)
+
+if __name__ == "__main__":
+    unittest.main()
