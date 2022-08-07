85a4997627fa89d87b9b7e326d5b32e7640b4b56
Nathan Thomas <nwthomas@me.com>
2022-08-06 20:00:00 -0600

    Add test scaffolding for new challenge

---
+++ src/leetcode/medium/continuous-subarray-sum/test_continuous_subarray_sum.py
@@ -0,0 +1,8 @@
+from continuous_subarray_sum import check_subarray_sum
+import unittest
+
+class TestCheckSubarraySum(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
