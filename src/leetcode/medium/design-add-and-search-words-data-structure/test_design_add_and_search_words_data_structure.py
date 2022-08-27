f52dd3d9ebbf24b0bc2873c37496fb7dd86b8cd7
Nathan Thomas <nwthomas@me.com>
2022-08-27 01:37:07 -0700

    Add scaffold testing file LC

---
+++ src/leetcode/medium/design-add-and-search-words-data-structure/test_design_add_and_search_words_data_structure.py
@@ -0,0 +1,8 @@
+from design_add_and_search_words_data_structure import WordDictionary
+import unittest
+
+class TestWordDictionary(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
