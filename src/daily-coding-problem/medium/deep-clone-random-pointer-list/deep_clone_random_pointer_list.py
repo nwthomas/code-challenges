db034e95ac76eaab1f002f2fde9d567456b55c97
Nathan Thomas <nwthomas@me.com>
2022-10-21 12:59:37 -0700

    Add draft solution for deep clone random sll pointer DCP

---
+++ src/daily-coding-problem/medium/deep-clone-random-pointer-list/deep_clone_random_pointer_list.py
@@ -0,0 +1,42 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This question was asked by Snapchat.
+
+Given the head to a singly linked list, where each node also has a “random” pointer that points to anywhere in the linked list, deep clone the list.
+"""
+
+class Node:
+    def __init__(self, value, next_node = None, random_node = None):
+        self.value = value
+        self.next_node = next_node
+        self.random_node = random_node
+
+def clone_random_pointer_list(l):
+    stack = [l]
+    cache = { l: Node(l.value) }
+
+    while len(stack) > 0:
+        current = stack.pop()
+        next_node = current.next_node
+        random_node = current.random_node
+
+        new_current = cache[current]
+
+        if next_node in cache:
+            new_current.next_node = cache[next_node]
+        elif next_node:
+            new_next_node = Node(next_node.value)
+            cache[next_node] = new_next_node
+            stack.append(next_node)
+            new_current.next_node = new_next_node
+
+        if random_node in cache:
+            new_current.random_node = cache[random_node]
+        elif random_node:
+            new_random_node = Node(random_node.value)
+            cache[random_node] = new_random_node
+            stack.append(random_node)
+            new_current.random_node = new_random_node
+
+    return cache[l]
