8c0890f613e0dfdca836417974e5fce3c2faf7ed
Nathan Thomas <nwthomas@me.com>
2022-11-05 20:00:00 -0600

    Add second test binary tree paths

--- src/daily-coding-problem/medium/binary-tree-paths/test_binary_tree_paths.py
+++ src/daily-coding-problem/medium/binary-tree-paths/test_binary_tree_paths.py
@@ -11,5 +11,19 @@ class TestGetBinaryTreePaths(unittest.TestCase):
         result = get_binary_tree_paths(root)
         self.assertEqual(result, [[10, 15], [10, 3]])
 
+    def test_gets_paths_for_larger_tree(self):
+        """Gets all the paths to leaves for a larger binary tree"""
+        root = TreeNode(10)
+        root.left = TreeNode(8)
+        root.left.left = TreeNode(5)
+        root.left.right = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right.right = TreeNode(25)
+        root.right.right.right = TreeNode(30)
+        root.right.right.left = TreeNode(24)
+
+        result = get_binary_tree_paths(root)
+        self.assertEqual(result, [[10, 20, 25, 30], [10, 20, 25, 24], [10, 8, 9], [10, 8, 5]])
+
 if __name__ == "__main__":
     unittest.main()
