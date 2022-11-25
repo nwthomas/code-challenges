0c4c219c9a3d83b25e6502e4b0904d07bfcc6895
Nathan Thomas <nwthomas@me.com>
2022-11-24 21:55:50 -0800

    Add tests for valid sedoku problem

--- src/leetcode/medium/valid-sedoku/test_valid_sedoky.py
+++ src/leetcode/medium/valid-sedoku/test_valid_sedoky.py
@@ -2,7 +2,39 @@ from valid_sedoku import isValidSudoku
 import unittest
 
 class TestIsValidSedoku(unittest.TestCase):
-    pass
+    invalidSedoku = [
+        [".",".",".",".","5",".",".","1","."],
+        [".","4",".","3",".",".",".",".","."],
+        [".",".",".",".",".","3",".",".","1"],
+        ["8",".",".",".",".",".",".","2","."],
+        [".",".","2",".","7",".",".",".","."],
+        [".","1","5",".",".",".",".",".","."],
+        [".",".",".",".",".","2",".",".","."],
+        [".","2",".","9",".",".",".",".","."],
+        [".",".","4",".",".",".",".",".","."],
+    ]
+
+    validSedoku = [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".",".","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+    ]
+
+    def test_is_valid_sedoku(self):
+        """Takes in a sedoku puzzle and returns True if it's valid"""
+        result = isValidSudoku(self.validSedoku)
+        self.assertTrue(result)
+
+    def test_is_invalid_sedoku(self):
+        """Takes in a sedoku puzzle and returns False if it's invalid"""
+        result = isValidSudoku(self.invalidSedoku)
+        self.assertFalse(result)
 
 if __name__ == "__main__":
     unittest.main()
