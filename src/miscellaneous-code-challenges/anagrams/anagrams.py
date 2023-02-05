f7845416d1d773a6fdb96cf6792a98b9d3e38107
Nathan Thomas <nwthomas@me.com>
2023-02-05 14:59:06 -0800

    Add anagrams_of misc challenge initial file

---
+++ src/miscellaneous-code-challenges/anagrams/anagrams.py
@@ -0,0 +1,18 @@
+"""
+Write a function that returns an array of all aanagrams of a given string. An anagram is a reordering of all characters within a string. 
+
+For example, the anagrams of "abc" are:
+[
+    "abc",
+    "acb",
+    "bac",
+    "bca",
+    "cab",
+    "cba"
+]
+"""
+
+from typing import List
+
+def anagrams_of(s: str) -> List[str]:
+    pass
