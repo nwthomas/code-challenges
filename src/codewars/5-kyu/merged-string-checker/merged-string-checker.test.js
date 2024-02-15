672f36f8d68a3c542755f2bbb9575a4863108656
Nathan Thomas <nwthomas@me.com>
2024-02-14 22:34:40 -0800

    Remove console.logs from merged string checker

--- src/codewars/5-kyu/merged-string-checker/merged-string-checker.test.js
+++ src/codewars/5-kyu/merged-string-checker/merged-string-checker.test.js
@@ -1,31 +1,31 @@
 const isMerge = require("./merged-string-checker.js");
 
 describe("Merged String Checker CodeWars 8kyu code challenge", () => {
-  describe("tests that two strings passed as an argument can combine to make the first string argument", () => {
-    it("should return true for two short strings arguments a-z and A-Z that form the first string argument", () => {
-      expect(isMerge("nathan", "nan", "ath")).toBeTruthy();
-    });
+    describe("tests that two strings passed as an argument can combine to make the first string argument", () => {
+        it("should return true for two short strings arguments a-z and A-Z that form the first string argument", () => {
+            expect(isMerge("nathan", "nan", "ath")).toBeTruthy();
+        });
 
-    it("should return true for two long strings of characters of all char types that form the first string argument", () => {
-      expect(isMerge("567SHklAY _*&^", "567lAY *^", "SHk_&")).toBeTruthy();
-    });
+        it("should return true for two long strings of characters of all char types that form the first string argument", () => {
+            expect(
+                isMerge("567SHklAY _*&^", "567lAY *^", "SHk_&"),
+            ).toBeTruthy();
+        });
 
-    it("should return false for two short strings of characters that do not form the first string argument", () => {
-      expect(isMerge("abcdefg", "yiosf&^", "98068797IJLHAKG")).toBeFalsy();
-    });
+        it("should return false for two short strings of characters that do not form the first string argument", () => {
+            expect(
+                isMerge("abcdefg", "yiosf&^", "98068797IJLHAKG"),
+            ).toBeFalsy();
+        });
 
-    it("should return false for two long strings of character that do not form the first string argument", () => {
-      expect(
-        isMerge(
-          "iuAHOGKGAJHHDOGHPOIJPJFN",
-          "^*&FUHPIUYuoHOIGFOPA*(&)&^",
-          "FUiolajhdsof_+*&A906f"
-        )
-      ).toBeFalsy();
+        it("should return false for two long strings of character that do not form the first string argument", () => {
+            expect(
+                isMerge(
+                    "iuAHOGKGAJHHDOGHPOIJPJFN",
+                    "^*&FUHPIUYuoHOIGFOPA*(&)&^",
+                    "FUiolajhdsof_+*&A906f",
+                ),
+            ).toBeFalsy();
+        });
     });
-  });
 });
-
-console.log(isMerge("codewars", "code", "wars")); // True
-console.log(isMerge("codewars", "cdw", "oears")); // True
-console.log(isMerge("codewars", "cod", "wars")); // False
