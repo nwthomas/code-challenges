2c199444ee44355d2a1b20306bb9fcd134b84ed1
Nathan Thomas <nwthomas@me.com>
2024-06-27 14:45:47 -0700

    Finish counting bits testing suite

--- src/leetcode/easy/counting-bits/countingBits.test.js
+++ src/leetcode/easy/counting-bits/countingBits.test.js
@@ -1,3 +1,17 @@
 const countBits = require("./countingBits");
 
-describe(countBits.name, () => {});
+describe(countBits.name, () => {
+    test("handles small n", () => {
+        const result = countBits(4);
+        expect(result).toEqual([0, 1, 1, 2, 1]);
+    });
+
+    test("handles large n", () => {
+        const result = countBits(50);
+        expect(result).toEqual([
+            0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3,
+            4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4,
+            4, 5, 2, 3, 3,
+        ]);
+    });
+});
