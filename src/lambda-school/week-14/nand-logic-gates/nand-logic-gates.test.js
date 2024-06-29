fa9979c3d4f16a9e5bd0ac1b8b249665bc9c3d27
Nathan Thomas <nwthomas@me.com>
2024-06-29 08:38:21 -0700

    Update spacing on tests for logic gates

--- src/lambda-school/week-14/nand-logic-gates/nand-logic-gates.test.js
+++ src/lambda-school/week-14/nand-logic-gates/nand-logic-gates.test.js
@@ -1,28 +1,32 @@
 const { NAND, AND, NOT, OR, XOR } = require("./nand-logic-gates");
 
 describe("the NAND gate library", () => {
-  describe("NAND() method", () => {
-    test("returns 0 if both inputs are truthy and 1 otherwise", () => {
-      expect(NAND(1, 1)).toBe(0);
-      expect(NAND(1, 0)).toBe(1);
+    describe("NAND() method", () => {
+        test("returns 0 if both inputs are truthy and 1 otherwise", () => {
+            expect(NAND(1, 1)).toBe(0);
+            expect(NAND(1, 0)).toBe(1);
+        });
     });
-  });
-  describe("AND() method", () => {
-    expect(AND(1, 0)).toBe(0);
-    expect(AND(0, 0)).toBe(0);
-    expect(AND(1, 1)).toBe(1);
-  });
-  describe("NOT() method", () => {
-    expect(NOT(1)).toBe(0);
-  });
-  describe("OR() method", () => {
-    expect(OR(1, 0)).toBe(1);
-    expect(OR(1, 1)).toBe(1);
-    expect(OR(0, 0)).toBe(0);
-  });
-  describe("XOR() method", () => {
-    expect(XOR(1, 1)).toBe(0);
-    expect(XOR(0, 0)).toBe(0);
-    expect(XOR(1, 0)).toBe(1);
-  });
+
+    describe("AND() method", () => {
+        expect(AND(1, 0)).toBe(0);
+        expect(AND(0, 0)).toBe(0);
+        expect(AND(1, 1)).toBe(1);
+    });
+
+    describe("NOT() method", () => {
+        expect(NOT(1)).toBe(0);
+    });
+
+    describe("OR() method", () => {
+        expect(OR(1, 0)).toBe(1);
+        expect(OR(1, 1)).toBe(1);
+        expect(OR(0, 0)).toBe(0);
+    });
+
+    describe("XOR() method", () => {
+        expect(XOR(1, 1)).toBe(0);
+        expect(XOR(0, 0)).toBe(0);
+        expect(XOR(1, 0)).toBe(1);
+    });
 });
