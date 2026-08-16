fe6ab1a648c8b5f0bf4dd32873d54068522e0abe
Nathan Thomas <nwthomas@me.com>
2026-08-16 12:16:01 -0700

    Add resumable iterator changes

--- src/miscellaneous-code-challenges/iterator/iterator.py
+++ src/miscellaneous-code-challenges/iterator/iterator.py
@@ -4,15 +4,52 @@ raise a StopIteration error when finished, and include capabilities to peak at t
 next item as well as 
 """
 
-from typing import Iterator, List, TypeVar, Union
-
-T = TypeVar('T')
+type State = dict[str, list["Iterator"] | int]
 
 
-class IteratorClass:
+class IteratorManager[T]:
+    state: State
+    # {
+    #   iterators: list[<iterator class>],
+    #   index: int,
+    # }
+
+    def __init__(self, state: State):
+        self.state = state
+
+    def __iter__(self) -> IteratorManager:
+        return self
+
+    def __next__(self) -> T:
+        currentIndex = self.state["index"]
+        iterators = self.state["iterators"]
+        if currentIndex >= len(iterators):
+            raise StopIteration
+
+        currentIterator = self.state["iterators"][currentIndex]
+        if currentIterator.index < len(currentIterator.values):
+            return currentIterator.__next__()
+
+        self.state["index"] += 1
+        if self.state["index"] >= len(iterators):
+            raise StopIteration
+
+        currentIndex = self.state["index"]
+        currentIterator = self.state["iterators"][currentIndex]
+        return currentIterator.__next__()
+
+    def getState(self) -> State:
+        return self.state
+
+    def setState(self, state: State):
+        self.state = state
+
+
+class Iterator[T]:
     index: int = 0
+    values: list[T]
 
-    def __init__(self, values: List[T]) -> None:
+    def __init__(self, values: list[T]) -> None:
         self.values = values
 
     def __iter__(self) -> Iterator:
@@ -26,7 +63,7 @@ class IteratorClass:
 
         raise StopIteration
 
-    def peak(self) -> Union[T, None] | None:
+    def peak(self) -> T | None:
         if self.index < len(self.values) - 1:
             return self.values[self.index + 1]
 
--- src/miscellaneous-code-challenges/iterator/test_iterator.py
+++ src/miscellaneous-code-challenges/iterator/test_iterator.py
@@ -1,11 +1,100 @@
-from iterator import IteratorClass
 import unittest
+
+from iterator import Iterator, IteratorManager
+
+
+class TestIteratorManager(unittest.TestCase):
+    def test_instantiates_manager_class(self):
+        """Instatiates with state"""
+        iterators = [Iterator([1, 2, 3, 4])]
+        state = {"index": 0, "iterators": iterators}
+        IteratorManager(state)
+
+    def test_gets_value_from_first_list(self):
+        """Instantiates and returns values from first iterator"""
+        iterators = [Iterator([1, 2, 3, 4]), Iterator([5, 6, 7, 8])]
+        state = {"index": 0, "iterators": iterators}
+        im = IteratorManager(state)
+        valueOne = im.__next__()
+        valueTwo = im.__next__()
+        self.assertEqual(valueOne, 1)
+        self.assertEqual(valueTwo, 2)
+
+    def test_starts_from_second_list(self):
+        """Instantiates and starts from second iterator in state"""
+        iterators = [Iterator([1, 2, 3, 4]), Iterator([5, 6, 7, 8])]
+        iterators[0].index = 4
+        state = {"index": 0, "iterators": iterators}
+        im = IteratorManager(state)
+        valueOne = im.__next__()
+        valueTwo = im.__next__()
+        valueThree = im.__next__()
+        valueFour = im.__next__()
+        self.assertEqual(valueOne, 5)
+        self.assertEqual(valueTwo, 6)
+        self.assertEqual(valueThree, 7)
+        self.assertEqual(valueFour, 8)
+
+        def raise_exception():
+            im.__next__()
+
+        with self.assertRaises(StopIteration):
+            raise_exception()
+
+    def test_iterators_across_iterators(self):
+        """Takes in multiple iterators and iterators across them"""
+        iterators = [Iterator([1, 2, 3, 4]), Iterator([5, 6, 7, 8])]
+        state = {"index": 0, "iterators": iterators}
+        im = IteratorManager(state)
+        valueOne = im.__next__()
+        valueTwo = im.__next__()
+        valueThree = im.__next__()
+        valueFour = im.__next__()
+        valueFive = im.__next__()
+        self.assertEqual(valueOne, 1)
+        self.assertEqual(valueTwo, 2)
+        self.assertEqual(valueThree, 3)
+        self.assertEqual(valueFour, 4)
+        self.assertEqual(valueFive, 5)
+
+    def test_iterators_can_stop_and_resume(self):
+        """Allows both stopping (getting) and resuming (setting) state"""
+        iterators = [Iterator([1, 2, 3, 4]), Iterator([5, 6, 7, 8])]
+        state = {"index": 0, "iterators": iterators}
+        im = IteratorManager(state)
+        valueOne = im.__next__()
+        self.assertEqual(valueOne, 1)
+
+        state = im.getState()
+
+        resumedIm = IteratorManager(state.copy())
+        valueTwo = resumedIm.__next__()
+        valueThree = resumedIm.__next__()
+        valueFour = resumedIm.__next__()
+        valueFive = resumedIm.__next__()
+        self.assertEqual(valueTwo, 2)
+        self.assertEqual(valueThree, 3)
+        self.assertEqual(valueFour, 4)
+        self.assertEqual(valueFive, 5)
+
+    def test_works_well_with_loops(self):
+        """Allows iteratoring with a loop"""
+        iterators = [Iterator([1, 2, 3, 4]), Iterator([5, 6, 7, 8])]
+        state = {"index": 0, "iterators": iterators}
+        im = IteratorManager(state)
+
+        result = []
+        for val in im:
+            result.append(val)
+
+        self.assertEqual(result, [1, 2, 3, 4, 5, 6, 7, 8])
+
 
 class TestIterator(unittest.TestCase):
     def test_iterates_on_int_list(self):
         """Takes in a list of ints and iterates on them"""
         l = [1, 2, 3, 4, 5]
-        iterator = IteratorClass(l)
+        iterator = Iterator(l)
 
         for i, val in enumerate(iterator):
             self.assertEqual(val, i + 1)
@@ -13,7 +102,7 @@ class TestIterator(unittest.TestCase):
     def test_iterates_on_function_list(self):
         """Takes in a list of functions and iterates on them"""
         l = [print, min, max]
-        iterator = IteratorClass(l)
+        iterator = Iterator(l)
 
         for i, val in enumerate(iterator):
             self.assertEqual(val, l[i])
@@ -21,11 +110,11 @@ class TestIterator(unittest.TestCase):
     def test_iterator_raises_stop_exception(self):
         """Takes in a list of values, iterates on them, and raises StopException"""
         l = ["test1", "test2"]
-        iterator = IteratorClass(l)
+        iterator = Iterator(l)
 
         for _ in iterator:
             pass
-        
+
         def raise_exception():
             iterator.__next__()
 
@@ -35,7 +124,7 @@ class TestIterator(unittest.TestCase):
     def test_iteator_peaks(self):
         """Allows peaking at next value if available, else returns None"""
         l = [1, 2, 3, 4, 5]
-        iterator = IteratorClass(l)
+        iterator = Iterator(l)
 
         next_value = iterator.peak()
         for i, _ in enumerate(iterator):
@@ -49,7 +138,7 @@ class TestIterator(unittest.TestCase):
     def test_restarts_iterator(self):
         """Allows iteration to end of iterable and then can restart iterable class"""
         l = [print, max, min]
-        iterator = IteratorClass(l)
+        iterator = Iterator(l)
 
         for _ in iterator:
             pass
